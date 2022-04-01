4d926cdf549c41644d5adf9f5eb13b41447346b3
mtnmr <mei.rila1013@gmail.com>
2022-04-01 17:34:43 +0900

    intent Action_view, show outputImage

--- android-workmanager-start_kotlin/app/src/main/java/com/example/background/BlurActivity.kt
+++ android-workmanager-start_kotlin/app/src/main/java/com/example/background/BlurActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.background
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
@@ -41,6 +42,15 @@ class BlurActivity : AppCompatActivity() {
         binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
 
         viewModel.outputWorkInfos.observe(this, workInfoObserver())
+
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
     }
 
 
@@ -54,6 +64,13 @@ class BlurActivity : AppCompatActivity() {
 
             if (workInfo.state.isFinished){
                 showWorkFinished()
+
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+                if (!outputImageUri.isNullOrEmpty()){
+                    viewModel.setOutputUri(outputImageUri as String)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                }
+
             }else{
                 showWorkInProgress()
             }
